247654a79e5012fbd1756465cc319e646eb3fb1a
mostafaelsayed <moelsayed949@gmail.com>
2024-08-13 20:52:57 +0300

    add .env

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const coursesRoutes = require("./routes/courseRoutes");
 const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 
 const app = express();
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-mongoose
-  .connect(
-    "mongodb+srv://moelsayed:onlinecourses%40123@onlinecourses.bfc9hky.mongodb.net/onlinecourses"
-  )
-  .then(() => console.log("Connected!"));
+mongoose .connect(process.env.MANGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("Connected!"));
 // Use logger middleware
 app.use(express.json());
 
